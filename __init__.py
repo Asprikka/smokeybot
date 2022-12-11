7588d64635b4035238fcc1f4b34c24c43346638c
capricavoid7@gmail.com <capricavoid7@gmail.com>
2022-12-11 21:44:22 +0200

    __init__ and __main__

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import logging
+
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
+
+LOGGER = logging.getLogger(__name__)
